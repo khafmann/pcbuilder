58e7f83a497ae4233b237bbdf2ed1c6301f48e5f
khafmann <khafmann@gmail.com>
2025-03-26 17:05:45 +0500

    Допилил логику поиска комплектующих

--- backend/src/services/build.service.ts
+++ backend/src/services/build.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, Between } from 'typeorm';
 import { GpuEntity } from '../entities/gpu.entity';
 import { CpuEntity } from '../entities/cpu.entity';
 import { MotherboardEntity } from '../entities/motherboard.entity';
@@ -26,35 +26,110 @@ export class BuildService {
     async generateBuild(budget: number, type: 'gaming' | 'workstation' | 'office') {
         let budgetDistribution = this.getBudgetDistribution(budget, type);
 
-        const cpu = await this.cpuRepo.findOne({ where: { price: budgetDistribution.cpu }, order: { price: 'DESC' } });
-        const motherboard = await this.motherboardRepo.findOne({ where: { price: budgetDistribution.motherboard, socket: cpu?.socket, ramType: cpu?.ramType }, order: { price: 'DESC' } });
-        const ram = await this.ramRepo.findOne({ where: { price: budgetDistribution.ram, ramType: cpu?.ramType }, order: { price: 'DESC' } });
-        const gpu = await this.gpuRepo.findOne({ where: { price: budgetDistribution.gpu }, order: { price: 'DESC' } });
-        const storage = await this.storageRepo.findOne({ where: { price: budgetDistribution.storage }, order: { price: 'DESC' } });
-        const psu = await this.psuRepo.findOne({ where: { price: budgetDistribution.psu, power: (cpu?.power ?? 0) + (gpu?.power ?? 0) }, order: { price: 'DESC' } });
-        const cooling = await this.coolingRepo.findOne({ where: { price: budgetDistribution.cooling, power: cpu?.power }, order: { price: 'DESC' } });
-        const pcCase = await this.caseRepo.findOne({ where: { price: budgetDistribution.case }, order: { price: 'DESC' } });
+        const cpu = await this.cpuRepo.findOne({
+            where: { price: Between(Math.round(budgetDistribution.cpu * 0), Math.round(budgetDistribution.cpu * 1.1)) },
+            order: { price: 'DESC' }
+        });
+
+        const motherboard = cpu ? await this.motherboardRepo.findOne({
+            where: {
+                price: Between(Math.round(budgetDistribution.motherboard * 0), Math.round(budgetDistribution.motherboard * 1.1)),
+                socket: cpu.socket,
+                ramType: cpu.ramType
+            },
+            order: { price: 'DESC' }
+        }) : null;
+
+        const ram = cpu ? await this.ramRepo.findOne({
+            where: {
+                price: Between(Math.round(budgetDistribution.ram * 0), Math.round(budgetDistribution.ram * 1.1)),
+                ramType: cpu.ramType
+            },
+            order: { price: 'DESC' }
+        }) : null;
+
+        const gpu = await this.gpuRepo.findOne({
+            where: { price: Between(Math.round(budgetDistribution.gpu * 0), Math.round(budgetDistribution.gpu * 1.1)) },
+            order: { price: 'DESC' }
+        });
+
+        const storage = await this.storageRepo.findOne({
+            where: { price: Between(Math.round(budgetDistribution.storage * 0), Math.round(budgetDistribution.storage * 1.1)) },
+            order: { price: 'DESC' }
+        });
+
+        const psus = await this.psuRepo.find({
+            where: {
+                price: Between(Math.round(budgetDistribution.psu * 0), Math.round(budgetDistribution.psu * 1.1)),
+            },
+            order: { price: 'DESC' }
+        });
+
+        // Расчет мощности ЦП и видеокарты
+        const psu = psus.find(p => p.power >= (cpu?.power ?? 0) + (gpu?.power ?? 0));
+
+        const coolings = await this.coolingRepo.find({
+            where: {
+                price: Between(Math.round(budgetDistribution.cooling * 0), Math.round(budgetDistribution.cooling * 1.1)),
+            },
+            order: { price: 'DESC' }
+        });
+
+        // Выбираем подходящее по мощности рассеивания охлаждение
+        const cooling = coolings.find(c => c.power >= (cpu?.power ?? 0)) ?? null;
+
+        const pcCase = await this.caseRepo.findOne({
+            where: { price: Between(Math.round(budgetDistribution.case * 0), Math.round(budgetDistribution.case * 1.1)) },
+            order: { price: 'DESC' }
+        });
 
         return { cpu, motherboard, ram, gpu, storage, psu, cooling, pcCase };
     }
 
     private getBudgetDistribution(budget: number, type: string) {
         const distributions = {
-            gaming: { gpu: 0.4, cpu: 0.25, motherboard: 0.1, ram: 0.1, storage: 0.05, psu: 0.05, cooling: 0.025, case: 0.025 },
-            workstation: { gpu: 0.3, cpu: 0.35, motherboard: 0.1, ram: 0.15, storage: 0.05, psu: 0.05, cooling: 0.025, case: 0.025 },
-            office: { gpu: 0.2, cpu: 0.3, motherboard: 0.15, ram: 0.15, storage: 0.1, psu: 0.05, cooling: 0.025, case: 0.025 },
+            gaming: {
+                gpu: 0.4,
+                cpu: 0.25,
+                motherboard: 0.1,
+                ram: 0.1,
+                storage: 0.05,
+                psu: 0.05,
+                cooling: 0.025,
+                case: 0.025
+            },
+            workstation: {
+                gpu: 0.3,
+                cpu: 0.35,
+                motherboard: 0.1,
+                ram: 0.15,
+                storage: 0.05,
+                psu: 0.05,
+                cooling: 0.025,
+                case: 0.025
+            },
+            office: {
+                gpu: 0.2,
+                cpu: 0.3,
+                motherboard: 0.15,
+                ram: 0.15,
+                storage: 0.1,
+                psu: 0.05,
+                cooling: 0.025,
+                case: 0.025
+            },
         };
 
         const selectedDistribution = distributions[type];
         return {
-            gpu: budget * selectedDistribution.gpu,
-            cpu: budget * selectedDistribution.cpu,
-            motherboard: budget * selectedDistribution.motherboard,
-            ram: budget * selectedDistribution.ram,
-            storage: budget * selectedDistribution.storage,
-            psu: budget * selectedDistribution.psu,
-            cooling: budget * selectedDistribution.cooling,
-            case: budget * selectedDistribution.case,
+            gpu: Math.round(budget * selectedDistribution.gpu),
+            cpu: Math.round(budget * selectedDistribution.cpu),
+            motherboard: Math.round(budget * selectedDistribution.motherboard),
+            ram: Math.round(budget * selectedDistribution.ram),
+            storage: Math.round(budget * selectedDistribution.storage),
+            psu: Math.round(budget * selectedDistribution.psu),
+            cooling: Math.round(budget * selectedDistribution.cooling),
+            case: Math.round(budget * selectedDistribution.case),
         };
     }
 }
