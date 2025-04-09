f3eff045883fb702b2d574180175ac323104307a
a.khafizov <a.khafizov@statdata.kz>
2025-04-09 15:09:31 +0500

    Рабочий паук кулеров

--- parsers/shop_cooling/shop_cooling/items.py
+++ parsers/shop_cooling/shop_cooling/items.py
@@ -6,7 +6,12 @@
 import scrapy
 
 
-class ShopCoolingItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class ParserItem(scrapy.Item):
+    id = scrapy.Field()
+    name = scrapy.Field()
+    price = scrapy.Field()
+    url = scrapy.Field()
+    store = scrapy.Field()
+    socket = scrapy.Field()
+    power = scrapy.Field()
     pass
